2bc44f69e98296aecb5532436dc82789f0c522c7
윤승현 <tkdeh339@gmail.com>
2021-08-24 04:21:53 +0900

    서버 초기화 작업을 위해서 임시로 개인정보가 올라간 파일들도 업로드 합니다.

---
+++ G_One_Flask_Server/g_one/module/mqtt_IDPW.py
@@ -0,0 +1,14 @@
+def Client():
+    return "Flask_MQTT"
+
+def Host():
+    return "gvsolgryn.nemiku.cc"
+
+def Port():
+    return "1883"
+
+def ID():
+    return "gvsolgryn"
+
+def PW():
+    return "tkdeh3554"
