2c3f0f0cc14fd911c4a8c5165e403f5231ba9903
GVSolgryn <tkdeh339@gmail.com>
2021-10-18 03:22:17 +0900

    Create G_One_HID_WPF

---
+++ G_One_HID_WPF/G_One/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G_One
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    /// 
+
+    using Module;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DB_Load()
+        {
+            int sensor_count = 0;
+            var sensor = string.Empty;
+            var status = string.Empty;
+
+            var db = new DB_Module();
+
+            string sql = "SELECT * FROM sensor_status";
+
+            try
+            {
+                var table = db.TableLoad(sql);
+
+                while (table.Read())
+                {
+                    sensor = table["sensor"].ToString();
+                    status = table["status"].ToString();
+                    sensor_count++;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void BtnAdd()
+        {
+            var btnTest = new Button
+            {
+                Name = "Btn_Add",
+                Content = "버튼 추가 테스트"
+            };
+
+            this.MainStackPanel.Children.Add(btnTest);
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            BtnAdd();
+        }
+
+        private void MainWindows_Load(object sender, RoutedEventArgs e)
+        {
+            DB_Load();
+        }
+
+    }
+}
---
+++ G_One_HID_WPF/G_One/Module/DB_Module.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace G_One.Module
+{
+    using MySql.Data.MySqlClient;
+    using System.Windows;
+
+    class DB_Module
+    {
+        public MySqlConnection Conn()
+        {
+            string server = "gone.gvsolgryn.de";
+            string user = "g_one";
+            string password = "g_one";
+            string database = "G_One_DB";
+            string port = "3306";
+
+            string connStr = $"Server={server};Port={port};User={user};Password={password};Database={database};SslMode=None;";
+
+            var conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+            return conn;
+        }
+
+        public MySqlCommand Command(string sql)
+        {
+            try
+            {
+                var cmd = new MySqlCommand(sql, Conn());
+
+                return cmd;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Command Error : " + ex.Message);
+                return null;
+            }
+        }
+
+        public MySqlDataReader TableLoad(string sql)
+        {
+            try
+            {
+                MySqlDataReader table = this.Command(sql).ExecuteReader();
+
+                return table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Table Load Error : " + ex.Message);
+
+                return null;
+            }
+        }
+
+        public MySqlCommand Execute(string sql, string[] parameter, string[] data)
+        {
+            try
+            {
+                var cmd = new MySqlCommand(sql, Conn())
+                {
+                    CommandText = sql
+                };
+
+                for (int i = 0; i < parameter.Length; i++)
+                {
+                    cmd.Parameters.AddWithValue(parameter[i], data[i]);
+                }
+
+                cmd.ExecuteNonQuery();
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                return null;
+            }
+        }
+    }
+}
