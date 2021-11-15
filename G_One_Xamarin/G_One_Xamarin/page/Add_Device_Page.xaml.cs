4d12931b58d0cecf5fd5d87b789e678d49099cdd
GVSolgryn <tkdeh339@gmail.com>
2021-11-16 03:47:30 +0900

    모달 페이지 추가
    Former-commit-id: d15e60ca188fbc6957fedcb5f9fbc19f4ab4a4a9

---
+++ G_One_Xamarin/G_One_Xamarin/page/Add_Device_Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace G_One_Xamarin.page
+{
+    public partial class Add_Device_Page : ContentPage
+    {
+        public Add_Device_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
