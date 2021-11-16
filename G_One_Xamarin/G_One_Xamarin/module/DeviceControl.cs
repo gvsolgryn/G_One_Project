220efc8030c99a7f1918b680ddeafffa77dd2c40
GVSolgryn <tkdeh339@gmail.com>
2021-11-17 03:51:36 +0900

    디바이스 컨트롤 모듈 수정
    Former-commit-id: c47f444de8c9d53100f7bc6b7a9a60aca4580547

--- G_One_Xamarin/G_One_Xamarin/module/DeviceControl.cs
+++ G_One_Xamarin/G_One_Xamarin/module/DeviceControl.cs
@@ -5,66 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
+using Xamarin.Forms;
+using M2Mqtt;
+
 namespace G_One_Xamarin.module
 {
     class DeviceControl
     {
+        private static readonly DbModule Db = new DbModule();
 
-        readonly static DB_Module db = new DB_Module();
-        readonly static MQTT_Module mqtt = new MQTT_Module();
-
-        static readonly List<DevicePanel> devicePanel = MainPage.devicePanel;
-
-        public static readonly List<string> listSensor = new List<string>();
-        public static readonly List<string> listStatus = new List<string>();
-        public static readonly List<string> listType = new List<string>();
-
-        public void StatusChange(int status, string name, string topic)
+        public static void StatusChange(int status, string name, string topic)
         {
+            MqttClient Client = new MqttClient("gone.gvsolgryn.de");
+            
             try
             {
-                string sql = "UPDATE sensor_status SET status = @sensorStatus, last_use = now() WHERE sensor = @sensorName";
-                db.Execute(sql, new[] { "@sensorStatus", "@sensorName" }, new[] { status.ToString(), name });
-                //mqtt.Publish(topic, status.ToString());
-                IconChange(status, name);
+                Client.Connect("G_One_Xamarin", "g_one", "g_one");
+                
+                const string sql = "UPDATE sensor_status SET status = @sensorStatus, last_use = now() WHERE sensor = @sensorName";
+                
+                Db.Execute(sql, new[] { "@sensorStatus", "@sensorName" }, new[] { status.ToString(), name });
+                
+                Client.Publish(topic, Encoding.UTF8.GetBytes(status.ToString()));
             }
             catch (Exception ex)
             {
                 //MessageBox.Show(ex.Message);
+                Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
             }
+
+            
         }
 
-        public void IconChange(int id, string name)
+        public static void LedValueChange(int value, string name, string topic)
         {
-            //int idx = devicePanel.FindIndex(x => x.DeviceName.Content.Equals(name));
-
-            string iconImagePath = String.Empty;
-
-            if (id == 1)
-            {
-
-                if (name.ToLower().Contains("led"))
-                {
-                    name = "led";
-                }
-
-                iconImagePath = name.ToLower() + "_on";
-                //devicePanel[idx].DeviceIconChange(iconImagePath);
-                //devicePanel[idx].DeviceButtonTextChange("끄기");
-            }
-
-            else if (id == 0)
-            {
-                if (name.ToLower().Contains("led"))
-                {
-                    name = "led";
-                }
-
-                iconImagePath = name.ToLower() + "_off";
-                //devicePanel[idx].DeviceIconChange(iconImagePath);
-                //devicePanel[idx].DeviceButtonTextChange("켜기");
-            }
+            
         }
-
     }
 }
